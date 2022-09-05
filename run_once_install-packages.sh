8a00d91ce56888a7fff2e9624687a5d23e759f42
Jaron Lundwall <13423952+jaron-l@users.noreply.github.com>
2022-09-05 18:48:48 +0000

    feat: add tmux to packages to install

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -5,5 +5,5 @@ then
 	sudo apt-get install -y curl
 fi
 curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.deb -o /tmp/nvim-linux64.deb
-sudo apt-get install -y zsh libglib2.0-dev git /tmp/nvim-linux64.deb
+sudo apt-get install -y zsh libglib2.0-dev git tmux /tmp/nvim-linux64.deb
 sudo chsh -s $(which zsh) $USER
