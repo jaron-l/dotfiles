bf44ea9f36217d11c475bb4575935177bf9f3e2a
Jaron Lundwall <13423952+jaron-l@users.noreply.github.com>
2022-09-05 18:50:34 +0000

    Change neovim to install from curled deb

---
+++ run_once_install-packages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+if [ ! "$(command -v curl)"; ]
+then
+	sudo apt-get install -y curl
+fi
+curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.deb -o /tmp/nvim-linux64.deb
+sudo apt-get install -y zsh libglib2.0-dev git /tmp/nvim-linux64.deb
+sudo chsh -s $(which zsh) $USER
