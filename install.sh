cb951e16e93dd3b757fded769426c2adaca22603
Jaron Lundwall <13423952+jaron-l@users.noreply.github.com>
2022-09-05 21:33:24 +0000

    fix: allow spaces in git email/name

--- install.sh
+++ install.sh
@@ -40,8 +40,8 @@ if [[ $YN == "y" || $YN == "Y" ]]
 then
 	read -e -p "git name: " -i "Jaron Lundwall" GITNAME
 	read -e -p "git email: " -i "13423952+jaron-l@users.noreply.github.com" GITEMAIL
-	git config --global user.name $GITNAME
-	git config --global user.email $GITEMAIL
+	git config --global user.name "$GITNAME"
+	git config --global user.email "$GITEMAIL"
 fi
 
 # install chezmoi
