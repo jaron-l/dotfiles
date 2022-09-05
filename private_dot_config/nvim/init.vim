599185ae95bc565b47289978980801f65043dc2a
Jaron Lundwall <13423952+jaron-l@users.noreply.github.com>
2022-09-05 19:32:56 +0000

    feat: port plugins to packer

--- private_dot_config/nvim/init.vim
+++ private_dot_config/nvim/init.vim
@@ -8,14 +8,4 @@
 :set softtabstop=4
 :set mouse=a
 
-call plug#begin()
-
-Plug 'https://github.com/preservim/nerdtree' " NerdTree
-Plug 'https://github.com/tpope/vim-commentary' " For Commenting gcc & gc
-Plug 'https://github.com/vim-airline/vim-airline' " Status bar
-Plug 'https://github.com/rafi/awesome-vim-colorschemes' " Retro Scheme
-Plug 'https://github.com/preservim/tagbar' " Tagbar for code navigation
-
-set encoding=UTF-8
-
-call plug#end()
+lua require('plugins')
---
+++ private_dot_config/nvim/lua/plugins.lua
@@ -0,0 +1,36 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use 'preservim/nerdtree' -- file management
+
+  use 'tpope/vim-commentary' -- For commenting gcc and gc
+
+  use 'vim-airline/vim-airline' -- status bar
+
+  use 'rafi/awesome-vim-colorschemes' -- retro scheme
+
+  use 'preservim/tagbar' --tag bar for code navigation
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+
+end)
